6c110e17eb074769a819ec3219373165da79d882
Jacob Bickerstaff <jacoblukebickerstaff@gmail.com>
2024-02-13 21:07:16 +0000

    started to replace conditions

--- constants/admission_types_tests_dataset.py
+++ constants/admission_types_tests_dataset.py
@@ -10,7 +10,10 @@ admission_mapping = {
     "emergency": {
         "injury_rtc": {"inpatient", "day_patient"},
         "self_inflicted": {"inpatient"},
-        "cardiology": {"inpatient", "day_patient"},
+        "cardiology": {
+            "stay_types": ["inpatient", "day_patient"],
+            "conditions": "cardiac"
+        },
         "neurology": {"inpatient"},
         "pulmonology": {"inpatient", "day_patient"},
         "infectious_diseases": {"inpatient", "outpatient"}
@@ -47,6 +50,247 @@ admission_mapping = {
     }
 }
 
+conditions = {
+    "infectious_diseases": [
+        "Common Cold",
+        "Influenza",
+        "Urinary Tract Infections (UTIs)",
+        "HIV/AIDS",
+        "Hepatitis",
+        "Tuberculosis",
+        "Malaria",
+        "COVID-19",
+        "Strep Throat",
+        "Herpes Simplex Virus"
+    ],
+    "cardiac": [
+        "Hypertension (High Blood Pressure)",
+        "Coronary Artery Disease",
+        "Heart Attack",
+        "Heart Failure",
+        "Arrhythmias",
+        "Peripheral Artery Disease",
+        "Stroke",
+        "Aneurysms",
+        "Venous Thromboembolism"
+    ],
+    "respiratory": [
+        "Asthma",
+        "Chronic Obstructive Pulmonary Disease (COPD)",
+        "Pneumonia",
+        "Bronchitis",
+        "Sinusitis",
+        "Allergic Rhinitis",
+        "Pulmonary Embolism",
+        "Lung Cancer",
+        "Cystic Fibrosis",
+        "Sleep Apnea"
+    ],
+    "digestive_system": [
+        "Gastroesophageal Reflux Disease (GERD)",
+        "Peptic Ulcer Disease",
+        "Irritable Bowel Syndrome (IBS)",
+        "Crohn's Disease",
+        "Ulcerative Colitis",
+        "Gallstones",
+        "Hepatitis",
+        "Cirrhosis",
+        "Pancreatitis",
+        "Celiac Disease"
+    ],
+    "endocrine_metabolic": [
+        "Diabetes Mellitus",
+        "Thyroid Disorders (Hypothyroidism, Hyperthyroidism)",
+        "Osteoporosis",
+        "Obesity",
+        "Metabolic Syndrome",
+        "Addison's Disease",
+        "Cushing's Syndrome",
+        "Gout",
+        "Polycystic Ovary Syndrome (PCOS)",
+        "Hyperlipidemia"
+    ],
+    "neurological": [
+        "Migraines",
+        "Epilepsy",
+        "Parkinson's Disease",
+        "Alzheimer's Disease",
+        "Multiple Sclerosis",
+        "Stroke",
+        "Peripheral Neuropathy",
+        "Bell's Palsy",
+        "Sciatica",
+        "Concussion"
+    ],
+    "psychiatric": [
+        "Depression",
+        "Anxiety Disorders",
+        "Bipolar Disorder",
+        "Schizophrenia",
+        "Post-Traumatic Stress Disorder (PTSD)",
+        "Obsessive-Compulsive Disorder (OCD)",
+        "Eating Disorders (Anorexia, Bulimia)",
+        "Attention Deficit Hyperactivity Disorder (ADHD)",
+        "Autism Spectrum Disorder",
+        "Insomnia"
+    ],
+    "musculoskeletal": [
+        "Arthritis (Osteoarthritis, Rheumatoid Arthritis)",
+        "Osteoporosis",
+        "Low Back Pain",
+        "Tendinitis",
+        "Fibromyalgia",
+        "Carpal Tunnel Syndrome",
+        "Gout",
+        "Lupus",
+        "Scoliosis",
+        "Fractures"
+    ],
+    "skin_conditions": [
+        "Acne",
+        "Eczema",
+        "Psoriasis",
+        "Dermatitis",
+        "Skin Cancer",
+        "Rosacea",
+        "Hives (Urticaria)",
+        "Impetigo",
+        "Cellulitis",
+        "Shingles (Herpes Zoster)"
+    ],
+    "hematologic": [
+        "Anemia",
+        "Hemophilia",
+        "Leukemia",
+        "Lymphoma",
+        "Sickle Cell Disease",
+        "Deep Vein Thrombosis (DVT)",
+        "Iron Deficiency Anemia",
+        "Thrombocytopenia",
+        "Hemochromatosis",
+        "Polycythemia Vera"
+    ],
+    "urological": [
+        "Urinary Tract Infections (UTIs)",
+        "Kidney Stones",
+        "Erectile Dysfunction",
+        "Prostate Enlargement (Benign Prostatic Hyperplasia)",
+        "Bladder Infections",
+        "Chronic Kidney Disease",
+        "Urinary Incontinence",
+        "Prostatitis",
+        "Overactive Bladder",
+        "Kidney Failure"
+    ],
+    "eye_vision": [
+        "Refractive Errors (Myopia, Hyperopia, Astigmatism)",
+        "Cataracts",
+        "Glaucoma",
+        "Macular Degeneration",
+        "Diabetic Retinopathy",
+        "Conjunctivitis (Pink Eye)",
+        "Dry Eye Syndrome",
+        "Uveitis",
+        "Retinal Detachment",
+        "Keratitis"
+    ],
+    "ear_nose_throat": [
+        "Otitis Media (Middle Ear Infection)",
+        "Otitis Externa (Swimmer's Ear)",
+        "Sinusitis",
+        "Tonsillitis",
+        "Laryngitis",
+        "Meniere's Disease",
+        "Tinnitus",
+        "Hearing Loss",
+        "Pharyngitis",
+        "Rhinitis"
+    ],
+    "dental_oral": [
+        "Dental Cavities",
+        "Gingivitis",
+        "Periodontitis",
+        "Oral Herpes",
+        "Oral Cancer",
+        "Temporomandibular Joint Disorders (TMJ)",
+        "Tooth Abscess",
+        "Halitosis (Bad Breath)",
+        "Oral Thrush",
+        "Impacted Wisdom Teeth"
+    ],
+    "reproductive_female": [
+        "Menstrual Disorders (Dysmenorrhea, Menorrhagia)",
+        "Polycystic Ovary Syndrome (PCOS)",
+        "Endometriosis",
+        "Uterine Fibroids",
+        "Ovarian Cysts",
+        "Pelvic Inflammatory Disease (PID)",
+        "Breast Cancer",
+        "Cervical Cancer",
+        "Vaginitis",
+        "Menopause Symptoms"
+    ],
+    "reproductive_male": [
+        "Erectile Dysfunction",
+        "Prostatitis",
+        "Benign Prostatic Hyperplasia (BPH)",
+        "Prostate Cancer",
+        "Testicular Cancer",
+        "Hydrocele",
+        "Varicocele",
+        "Infertility",
+        "Orchitis",
+        "Peyronie's Disease"
+    ],
+    "genetic_disorders": [
+        "Down Syndrome",
+        "Cystic Fibrosis",
+        "Sickle Cell Disease",
+        "Hemophilia",
+        "Huntington's Disease",
+        "Duchenne Muscular Dystrophy",
+        "Thalassemia",
+        "Fragile X Syndrome",
+        "Turner Syndrome",
+        "Marfan Syndrome"
+    ],
+    "autoimmune_diseases": [
+        "Rheumatoid Arthritis",
+        "Lupus",
+        "Type 1 Diabetes",
+        "Multiple Sclerosis",
+        "Psoriasis",
+        "Inflammatory Bowel Disease (IBD)",
+        "Hashimoto's Thyroiditis",
+        "Graves' Disease",
+        "Celiac Disease",
+        "Sjögren's Syndrome"
+    ],
+    "oncological_conditions": [
+        "Breast Cancer",
+        "Lung Cancer",
+        "Prostate Cancer",
+        "Colorectal Cancer",
+        "Melanoma",
+        "Leukemia",
+        "Lymphoma",
+        "Pancreatic Cancer",
+        "Ovarian Cancer",
+        "Bladder Cancer"
+    ],
+    "allergies_immune_disorders": [
+        "Allergic Rhinitis (Hay Fever)",
+        "Food Allergies",
+        "Drug Allergies",
+        "Asthma",
+        "Eczema (Atopic Dermatitis)",
+        "Anaphylaxis",
+        "Autoimmune Diseases",
+        "Immunodeficiency Disorders",
+        "Contact Dermatitis",
+        "Latex Allergy"
+    ]
+}
 
 stay_type = ["outpatient", "inpatient", "day_patient"]
 
--- utils/util_funcs.py
+++ utils/util_funcs.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Union
 
 from IPython.core.display_functions import display
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, count
 
 
 def get_row_count(df: DataFrame, verbose=False):
@@ -26,3 +27,49 @@ def get_column_names(df: DataFrame):
     column_names = df.columns
     print(column_names)
 
+def remove_data(df_driver: DataFrame, df_anti: DataFrame, condition_1: Union[bool, col], condition_2: Union[bool,col]):
+    result_df = df_driver.join(df_anti,
+                               on=['sub_level_admission', 'DOB'],
+                               how='left_anti')
+
+    assert result_df.where(condition_1 & condition_2).count() == 0
+    return result_df
+
+
+def verify_ranking(_df: DataFrame, ranked_df: DataFrame):
+    joined_df = _df.alias("df").join(
+        ranked_df.alias("ranked"),
+
+        (col("df.unique_id") == col("ranked.unique_id")),
+        "inner"
+    )
+
+    verification_df = joined_df.select(
+        col("df.unique_id")
+    ).distinct().groupBy("unique_id").agg(
+        count("unique_id").alias("unique_id_count")
+    )
+
+    # Check if any name has a mismatch in the count of unique IDs
+    mismatch_count = verification_df.filter("unique_id_count > 1").count()
+
+    # Assert that there are no mismatches
+    assert mismatch_count == 0, f"There are names with mismatched unique ID counts. mismatch_count: {mismatch_count}"
+
+def verify_ranking_counts(_df: DataFrame, ranked_df: DataFrame, names: list, unique_ids: list):
+    for name, unique_id in zip(names, unique_ids):
+        # Define filter conditions
+        name_condition = col("name") == name
+        unique_id_condition = col("unique_id") == unique_id
+        stay_name_condition = col("stay_name").like(f"{name}_%")
+
+        df_filtered_count = (_df
+                             .filter(name_condition & unique_id_condition & stay_name_condition)
+                             .count())
+
+        ranked_df_filtered_count = (ranked_df
+                                    .filter(name_condition & unique_id_condition & stay_name_condition)
+                                    .count())
+
+        # Assert that the counts are equal for each name-unique_id pair
+        assert df_filtered_count == ranked_df_filtered_count, f"The number of matching rows for {name} with unique ID {unique_id} should be the same in both DataFrames."
