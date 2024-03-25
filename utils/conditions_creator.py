edc0a85ea8b367a4150eafb248d7ae97d0662ec8
Jacob Bickerstaff <jacoblukebickerstaff@gmail.com>
2024-03-25 22:29:23 +0000

    added stay types

--- utils/conditions_creator.py
+++ utils/conditions_creator.py
@@ -125,6 +125,7 @@ class ConditionsCreator:
                                                                         col("condition"),
                                                                         col("is_pediatric"),
                                                                         col("unique_id"),
+                                                                        col("stay_types"),
                                                                         col("condition_admission_type"),
                                                                         col("top_level_admission")
                                                                     )
@@ -158,10 +159,10 @@ class ConditionsCreator:
             unique_id = row["unique_id"]
             probability_entries = [((entry.age_min, entry.age_max), entry.probability) for entry in
                                    row["probability_entries"]]
-            row_infos = [(entry.Age, entry.condition, entry.is_pediatric, unique_id, entry.condition_admission_type,
+            row_infos = [(entry.Age, entry.condition, entry.is_pediatric, entry.unique_id, entry.stay_types, entry.condition_admission_type,
                           entry.top_level_admission) for entry in row["row_infos"]]
             for probability_info, patient_info in zip(probability_entries, row_infos):
-                age, condition, is_pediatric, unique_id, condition_admission_type, top_level_admission = patient_info
+                age, condition, is_pediatric, unique_id, stay_types, condition_admission_type, top_level_admission = patient_info
                 age_prob = probability_info
 
                 prob = self._find_probability_for_age_gender(age, age_prob)
@@ -174,7 +175,7 @@ class ConditionsCreator:
                 conditions_probabilities.append(
                     (f"{unique_id}_{top_level_admission}_{condition_admission_type}", condition_label, prob))
                 patient_name = unique_id.split('_')[0]
-                patients_conditions[patient_name].append((unique_id, top_level_admission, condition_label, prob))
+                patients_conditions[patient_name].append((unique_id, stay_types, top_level_admission, condition_label, prob))
 
         # Sort the conditions by probability for easier handling (optional)
         conditions_probabilities.sort(key=lambda x: x[1], reverse=True)
@@ -186,14 +187,14 @@ class ConditionsCreator:
             while not condition_selected:
                 random_prob = np.random.uniform(0, total_prob)
                 cumulative_prob = 0
-                for uniq_id, top_level, condition, prob in conditions:
+                for uniq_id, stay_types, top_level, condition, prob in conditions:
                     cumulative_prob += prob
                     if random_prob < cumulative_prob:
-                        chosen_conditions_list.append((uniq_id, top_level, condition))
+                        chosen_conditions_list.append((uniq_id, stay_types, top_level, condition))
                         condition_selected = True
                         break  # Stop after selecting one condition for the current patient
         return self._spark.createDataFrame(chosen_conditions_list,
-                                           ("unique_id", "chosen_top_level_admission", "chosen_condition"))
+                                           ("unique_id", "stay_types", "chosen_top_level_admission", "chosen_condition"))
 
     def runner(self):
         return self._choose_conditions_for_patients()
