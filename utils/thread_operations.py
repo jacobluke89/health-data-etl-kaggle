2a18700273cb99ed3aaa40073db3ea72278be727
Jacob Bickerstaff <jacoblukebickerstaff@gmail.com>
2024-04-07 10:51:50 +0100

    doc strings added.

--- utils/thread_operations.py
+++ utils/thread_operations.py
@@ -1,14 +1,28 @@
+from functools import reduce
+from typing import Type, Any
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import spark_partition_id
-from typing import Type
-from functools import reduce
+
 
 class TaskWrapper:
     def __init__(self, spark, task_class, *args, **kwargs):
+        """
+
+        Args:
+            spark (SparkSession): The SparkSession
+            task_class: The class that's going to be run.
+            *args: The args associated with class.
+            **kwargs: The kwargs associated with class, if any.
+        """
         self.task = task_class(spark, *args, **kwargs)
 
-    def runner(self):
+    def runner(self) -> Any:
+        """
+        Calls the task runner function
+        Returns:
+            Any: returns Any type, right now DataFrame
+        """
         return self.task.runner()
 
 
@@ -18,6 +32,18 @@ def process_partition(spark: SparkSession,
                       df: DataFrame,
                       *args,
                       **kwargs) -> DataFrame:
+    """
+    This processes the partition result of the spark_parition process result from the TaskWrapper.runner()
+    Args:
+        spark (SparkSession): The SparkSession
+        partition_id (int): The partition id to filter against the df
+        class_to_run (Type): The class to run
+        df (DataFrame): The partitioned df 
+        *args: The args associated with class.
+        **kwargs: The kwargs associated with class, if any.
+    Returns:
+        Dataframe: The Dataframe result. 
+    """
     # Filter the DataFrame for this partition's data
     partition_df = df.filter(spark_partition_id() == partition_id)
 
@@ -27,12 +53,25 @@ def process_partition(spark: SparkSession,
     result_df = task.runner()
     return result_df
 
+
 def run_multithreaded_processing(spark: SparkSession,
                                  class_to_run: Type,
                                  num_partitions: int,
                                  df: DataFrame,
                                  *args,
                                  **kwargs) -> DataFrame:
+    """
+    Runs the Threaded Processing Process
+    Args:
+        spark (SparkSession): The SparkSession
+        class_to_run (Type): The class to run
+        num_partitions (int): total number of partitions to iterate over 
+        df (DataFrame): The partitioned df 
+        *args: The args associated with class.
+        **kwargs: The kwargs associated with class, if any.
+    Returns:
+       DataFrame: The unionised DataFrame
+    """
     partitioned_dfs = []
 
     with ThreadPoolExecutor(max_workers=num_partitions) as executor:
@@ -48,13 +87,22 @@ def run_multithreaded_processing(spark: SparkSession,
     if partitioned_dfs:
         final_df = reduce(DataFrame.unionByName, partitioned_dfs)
         return final_df
-    else:
-        return spark.createDataFrame([], args[0].schema)
+    return spark.createDataFrame([], args[0].schema)
 
-def thread_runner(spark: SparkSession,
-                  class_to_run: Type,
-                  *args,
-                  **kwargs) -> DataFrame:
+
+def runner(spark: SparkSession,
+           class_to_run: Type,
+           *args,
+           **kwargs) -> DataFrame:
+    """
+    Args:
+        spark (SparkSession): The SparkSession
+        class_to_run (Type): The class to run
+        *args: The args associated with class.
+        **kwargs: The kwargs associated with class, if any.
+    Returns:
+        DataFrame: The returned DataFrame from the class to run.
+    """
     df = args[0]  # Assuming the first positional argument is always the DataFrame
     total_rows = df.count()
     rows_per_partition = 100
